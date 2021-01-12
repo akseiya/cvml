5fc7ddc7cc4809925c36daaef8ef87b514eae4b4
Michał Lech <akseiya@gmail.com>
2021-01-12 13:50:32 +0100

    renderer (just yaml.stringify) as a module

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const { render } = require('./lib/render')
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
 const port = 3000
 
 app.use(express.static('files'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello World!')
+app.post('/render', (req, res) => {
+    res.send(`<pre>\n${render(req)}\n</pre>`)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
